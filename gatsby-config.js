e9e031548f8d32ace20cc033d320024791900c14
tubone24 <tubo.yyyuuu@gmail.com>
2019-12-12 00:12:19 +0900

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,8 @@ module.exports = {
               // will be rendered at 50px.
               //
               // Defaults to false.
-              sizeByPixelDensity: false
+              sizeByPixelDensity: false,
+              withWebp: true,
             }
           }
         ]
