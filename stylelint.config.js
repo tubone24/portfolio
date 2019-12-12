3dec629b58248aadc045c0dd729a48589fabbd26
tubone24 <tubo.yyyuuu@gmail.com>
2019-12-12 21:00:21 +0900

    styled lint

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'stylelint-config-styled-components',
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+  ],
+  "processors": ['stylelint-processor-styled-components'],
+};
