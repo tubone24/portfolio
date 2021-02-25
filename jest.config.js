0410fca1cc9aa4bf5d762f3f252ae9a17bbd3125
tubone <tubo.yyyuuu@gmail.com>
2021-02-25 23:53:09 +0900

    ignore cypress path

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/jest-configs/__mocks__/file-mocks.js`,
   },
-  testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
+  testPathIgnorePatterns: [`node_modules`, `.cache`, `public`, `cypress`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`, `\\.svg`],
   globals: {
     __PATH_PREFIX__: ``,
