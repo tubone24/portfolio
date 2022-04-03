6ec0eb2ae813f2beddbc025c597ef82a8fd51625
tubone24 <tubo.yyyuuu@gmail.com>
2022-04-03 13:55:10 +0900

    WIP alt

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import NukaCarousel from "nuka-carousel";
 
 type Props = {
-  images: string[];
+  images: Image[];
+};
+
+type Image = {
+  src: string;
+  title?: string;
 };
 
 export const Carousel = (props: Props): JSX.Element => {
   const images = props.images.map((image) => {
-    return <img key={image} src={image} alt={image} />;
+    return (
+      <img
+        key={image.src}
+        src={image.src}
+        alt={image.title || image.src}
+        title={image.title || ""}
+      />
+    );
   });
 
   return (
